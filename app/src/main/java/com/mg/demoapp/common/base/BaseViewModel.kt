778eb0a33caedc1c4273ba8ebe1703ed2f743f6b
Maga <maga@x-tech.am>
2020-04-08 16:40:47 +0400

    Add error handeling

--- app/src/main/java/com/mg/demoapp/common/base/BaseViewModel.kt
+++ app/src/main/java/com/mg/demoapp/common/base/BaseViewModel.kt
@@ -1,11 +1,14 @@
 package com.mg.demoapp.common.base
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import androidx.navigation.NavDirections
+import com.mg.demoapp.R
 import com.mg.demoapp.common.utils.Event
+import com.mg.demoapp.data.repository.AppDispatchers
+import com.mg.demoapp.data.repository.utils.Resource
 import com.mg.demoapp.navigation.NavigationCommand
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 abstract class BaseViewModel: ViewModel() {
 
@@ -17,10 +20,48 @@ abstract class BaseViewModel: ViewModel() {
     private val _navigation = MutableLiveData<Event<NavigationCommand>>()
     val navigation: LiveData<Event<NavigationCommand>> = _navigation
 
+    private var status = MediatorLiveData<Resource.Status>()
+    private val statusObserver: androidx.lifecycle.Observer<Resource.Status> =
+        androidx.lifecycle.Observer {}
+
+    init {
+        status.observeForever { statusObserver }//statusObserver's onChanged() method won't be  called without active observer
+    }
+
+
     /**
      * Convenient method to handle navigation from a [ViewModel]
      */
     fun navigate(directions: NavDirections) {
         _navigation.value = Event(NavigationCommand.To(directions))
     }
+
+    fun <T> handleRequest(
+        dispatchers: AppDispatchers,
+        request: suspend () -> LiveData<Resource<T>>,
+        callback: (LiveData<Resource<T>>) -> Unit
+    ) {
+        viewModelScope.launch(dispatchers.main) {
+            val result: LiveData<Resource<T>> = withContext(dispatchers.io) {
+                request()
+            }
+            status.addSource(Transformations.map(result) { _result -> _result.status }) { _status ->
+                status.value = _status
+                handleError(_status)
+            }
+            callback(result)
+
+        }
+    }
+
+    private fun handleError(status: Resource.Status) {
+        if (status == Resource.Status.ERROR) _snackbarError.value =
+            Event(R.string.an_error_happened)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        status.removeObserver(statusObserver)
+    }
 }
+
--- app/src/main/java/com/mg/demoapp/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/mg/demoapp/ui/splash/SplashViewModel.kt
@@ -1,9 +1,6 @@
 package com.mg.demoapp.ui.splash
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.mg.demoapp.R
 import com.mg.demoapp.common.base.BaseViewModel
 import com.mg.demoapp.common.utils.Event
@@ -15,28 +12,37 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 
-class SplashViewModel(private val getSplashlUseCase: GetSplashUseCase,
-                      private val dispatchers: AppDispatchers
+class SplashViewModel(
+    private val getSplashlUseCase: GetSplashUseCase,
+    private val dispatchers: AppDispatchers
 ) : BaseViewModel() {
 
     private var splashSource: LiveData<Resource<Splash>> = MutableLiveData()
     private val _splash = MediatorLiveData<Splash>()
     val splash: LiveData<Splash> get() = _splash
+    private val shamObserver: Observer<Splash> = Observer { }
 //    private val _isLoading = MutableLiveData<Resource.Status>()
+
+    init {
+        _splash.observeForever(shamObserver)
+    }
 
     fun loadDataWhenActivityStarts() {
         getSplash()
     }
 
-    private fun getSplash() = viewModelScope.launch(dispatchers.main) {
+    private fun getSplash() = handleRequest(dispatchers, { getSplashlUseCase() }) { result ->
         _splash.removeSource(splashSource) // We make sure there is only one source of livedata (allowing us properly refresh)
-        withContext(dispatchers.io) {splashSource = getSplashlUseCase()}
-        _splash.addSource(splashSource){
+        splashSource = result
+        _splash.addSource(splashSource) {
             _splash.value = it.data
-//            _isLoading.value = it.status
             if (it.status == Resource.Status.SUCCESS) navigate(SplashFragmentDirections.actionSplashFragmentToLoginFragment())
-            if (it.status == Resource.Status.ERROR) _snackbarError.value = Event(R.string.an_error_happened)
         }
     }
 
+
+    override fun onCleared() {
+        super.onCleared()
+        _splash.removeObserver(shamObserver)
+    }
 }
